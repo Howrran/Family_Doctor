2a3032d3d6db87cbe88cb5ea7739952612dbc3a3
Mykola Matviiv <infiniti943@gmail.com>
2019-05-08 01:06:23 +0300

    boom

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\IDE\PyCharm 2017.3.4\projects\Family_Doctor\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
